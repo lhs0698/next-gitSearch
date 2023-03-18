cb6cd9267fafd396c048791af26f8d7ec9fa85bb
lhs0698 <yohs0698@naver.com>
2023-03-18 19:15:17 +0900

    github 계정 검색 시 보여주기

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+const App = () => {
+  const [username, setUsername] = useState("");
+
+  return (
+    <div>
+      <label>
+        username
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <p>깃 허브 검색하기</p>
+      <Link href={`/users/${username}`}>검색하기</Link>
+    </div>
+  );
+};
+
+export default App;
